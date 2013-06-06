2f39e6254dfdeb3e76927d2afd63be8a522f012f
Mark Smith <markmsmith@hotmail.com>
2013-06-06 03:00:42 -0400

    Added a package.json for dependencies.  Added jquery and lodash.  Add touch support with throttling.

--- public/client.js
+++ public/client.js
@@ -1,70 +1,112 @@
 (function() {
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   window.Fingerpaint = {};
+
   Fingerpaint.Client = (function() {
     function Client() {
+      var _this = this;
+
       this.users = {};
       this.socket = io.connect("http://" + document.location.hostname + "/");
       this.canvas = $('#draw');
       this.viewport = $(window);
       this.resizeCanvas();
-      this.viewport.resize(__bind(function() {
-        return this.resizeCanvas();
-      }, this));
-      this.socket.on('hello', __bind(function(me, users) {
+      this.viewport.resize(function() {
+        return _this.resizeCanvas();
+      });
+      this.socket.on('hello', function(me, users) {
         var id, user, _results;
-        this.me = me;
+
+        _this.me = me;
         _results = [];
         for (id in users) {
           user = users[id];
-          _results.push(this.addUser(user));
+          _results.push(_this.addUser(user));
         }
         return _results;
-      }, this));
-      this.socket.on('join', __bind(function(user) {
-        return this.addUser(user);
-      }, this));
-      this.socket.on('part', __bind(function(user) {
-        return this.removeUser(user);
-      }, this));
-      this.socket.on('move', __bind(function(id, position, drawing) {
+      });
+      this.socket.on('join', function(user) {
+        return _this.addUser(user);
+      });
+      this.socket.on('part', function(user) {
+        return _this.removeUser(user);
+      });
+      this.socket.on('move', function(id, position, drawing) {
         var user;
-        user = this.users[id];
-        return this.moveUser(user, position, drawing);
-      }, this));
-      this.socket.on('nick', __bind(function(id, nick) {
+
+        user = _this.users[id];
+        return _this.moveUser(user, position, drawing);
+      });
+      this.socket.on('nick', function(id, nick) {
         var user;
-        user = this.users[id];
-        return this.changeNick(user, nick);
-      }, this));
-      $(document).mousemove(__bind(function(event) {
+
+        user = _this.users[id];
+        return _this.changeNick(user, nick);
+      });
+      $(document).mousemove(_.throttle(function(event) {
         var position;
+
+        console.log('mousemove');
         position = {
           x: event.pageX,
           y: event.pageY
         };
-        return this.socket.json.emit('move', position, this.drawing);
-      }, this));
-      $(document).mousedown(__bind(function(event) {
-        return this.drawing = true;
-      }, this));
-      $(document).mouseup(__bind(function(event) {
-        return this.drawing = false;
-      }, this));
-      $(document).keyup(__bind(function(event) {
+        return _this.handleMove(position);
+      }, 50));
+      $(document).mousedown(function(event) {
+        console.log('mousedown');
+        return _this.drawing = true;
+      });
+      $(document).mouseup(function(event) {
+        console.log('mouseup');
+        return _this.drawing = false;
+      });
+      $(document).on({
+        'touchstart': function(event) {
+          console.log('touchstart');
+          return _this.drawing = true;
+        }
+      });
+      $(document).on({
+        'touchend': function(event) {
+          console.log('touchend');
+          return _this.drawing = false;
+        }
+      });
+      $(document).on({
+        'touchmove': _.throttle(function(event) {
+          var position, touch;
+
+          console.log('touchmove');
+          event.preventDefault();
+          touch = event.originalEvent.touches[0];
+          position = {
+            x: touch.pageX,
+            y: touch.pageY
+          };
+          return _this.handleMove(position);
+        }, 50)
+      });
+      $(document).keyup(function(event) {
         var nick;
+
         if (event.keyCode === 78) {
           nick = prompt("what's your name?");
-          return this.socket.emit('nick', nick);
+          return _this.socket.emit('nick', nick);
         }
-      }, this));
+      });
     }
+
+    Client.prototype.handleMove = function(position) {
+      return this.socket.json.emit('move', position, this.drawing);
+    };
+
     Client.prototype.resizeCanvas = function() {
       return this.canvas.attr({
         width: this.viewport.width(),
         height: this.viewport.height()
       });
     };
+
     Client.prototype.addUser = function(user) {
       this.users[user.id] = {
         id: user.id,
@@ -73,14 +115,18 @@
       };
       return this.updateStatus();
     };
+
     Client.prototype.changeNick = function(user, nick) {
       user.nick = nick;
       return $('.nick', user.avatar).html(nick);
     };
+
     Client.prototype.updateStatus = function() {
       var count, key;
+
       count = ((function() {
         var _results;
+
         _results = [];
         for (key in this.users) {
           _results.push(key);
@@ -89,15 +135,19 @@
       }).call(this)).length;
       return $('#status').html("" + count + " " + (count === 1 ? 'user' : 'users') + " connected");
     };
+
     Client.prototype.removeUser = function(user) {
       var avatar;
+
       avatar = this.users[user.id].avatar;
       avatar.remove();
       delete this.users[user.id];
       return this.updateStatus();
     };
+
     Client.prototype.moveUser = function(user, position, drawing) {
       var ctx, offset, old;
+
       if (drawing) {
         offset = user.avatar.position();
         old = {
@@ -118,8 +168,10 @@
         top: "" + (position.y - 8) + "px"
       });
     };
+
     Client.prototype.createAvatar = function(user) {
       var avatar, canvas, ctx, nick;
+
       avatar = $("<div class='avatar' id='user-" + user.id + "'/>").appendTo('body');
       canvas = $('<canvas/>').attr({
         width: 16,
@@ -138,6 +190,9 @@
       nick.css('color', "rgba(" + user.color + ", 1)");
       return $(avatar);
     };
+
     return Client;
+
   })();
+
 }).call(this);
--- src/client.coffee
+++ src/client.coffee
@@ -1,82 +1,107 @@
 window.Fingerpaint = {}
 
 class Fingerpaint.Client
-	
+
 	constructor: ->
-		
+
 		@users = {}
 		@socket = io.connect "http://#{document.location.hostname}/"
-		
+
 		@canvas = $('#draw')
 		@viewport = $(window)
-		
+
 		@resizeCanvas()
 		@viewport.resize =>
 			@resizeCanvas()
-		
+
 		@socket.on 'hello', (me, users) =>
 			@me = me
 			@addUser(user) for id, user of users
-		
+
 		@socket.on 'join', (user) =>
 			@addUser(user)
-		
+
 		@socket.on 'part', (user) =>
 			@removeUser(user)
-			
+
 		@socket.on 'move', (id, position, drawing) =>
 			user = @users[id]
 			@moveUser user, position, drawing
-		
+
 		@socket.on 'nick', (id, nick) =>
 			user = @users[id]
 			@changeNick user, nick
-		
-		$(document).mousemove (event) =>
+
+		$(document).mousemove _.throttle((event) =>
+			console.log('mousemove')
 			position =
 				x: event.pageX
 				y: event.pageY
-			@socket.json.emit 'move', position, @drawing
-		
+			@handleMove position
+		, 50)
+
 		$(document).mousedown (event) =>
+			console.log('mousedown')
 			@drawing = true
-		
+
 		$(document).mouseup (event) =>
+			console.log('mouseup')
 			@drawing = false
-			
+
+		$(document).on 'touchstart': (event) =>
+			console.log('touchstart')
+			@drawing = true
+
+		$(document).on 'touchend': (event) =>
+			console.log('touchend')
+			@drawing = false
+
+		$(document).on 'touchmove': _.throttle((event) =>
+			console.log('touchmove')
+			event.preventDefault();
+			touch = event.originalEvent.touches[0];
+			position =
+				x: touch.pageX
+				y: touch.pageY
+			@handleMove position
+		, 50)
+
 		$(document).keyup (event) =>
 			if event.keyCode is 78
 				nick = prompt "what's your name?"
 				@socket.emit 'nick', nick
-	
+
+	handleMove: (position) ->
+		@socket.json.emit 'move', position, @drawing
+
 	resizeCanvas: ->
 		@canvas.attr
 			width: @viewport.width()
 			height: @viewport.height()
-	
+
 	addUser: (user) ->
 		@users[user.id] =
 			id:     user.id
 			color:  user.color
 			avatar: @createAvatar(user)
 		@updateStatus()
-	
+
 	changeNick: (user, nick) ->
 		user.nick = nick
 		$('.nick', user.avatar).html(nick)
-	
+
 	updateStatus: ->
 		count = (key for key of @users).length
 		$('#status').html("#{count} #{if count is 1 then 'user' else 'users'} connected")
-	
+
 	removeUser: (user) ->
 		avatar = @users[user.id].avatar
 		avatar.remove()
 		delete @users[user.id]
 		@updateStatus()
-	
+
 	moveUser: (user, position, drawing) ->
-		
+
 		if drawing
 			offset = user.avatar.position()
 
@@ -92,16 +117,16 @@ class Fingerpaint.Client
 			ctx.lineTo(position.x, position.y)
 			ctx.closePath()
 			ctx.stroke()
-		
+
 		user.avatar.css
 			left: "#{position.x - 8}px"
 			top:  "#{position.y - 8}px"
-	
+
 	createAvatar: (user) ->
-		
+
 		avatar = $("<div class='avatar' id='user-#{user.id}'/>").appendTo('body')
 		canvas = $('<canvas/>').attr(width: 16, height: 16).appendTo(avatar)
-		
+
 		ctx = canvas.get(0).getContext('2d')
 		ctx.lineWidth = 0.5
 		ctx.fillStyle = "rgba(#{user.color}, 0.2)"
@@ -111,8 +136,8 @@ class Fingerpaint.Client
 		ctx.closePath()
 		ctx.fill()
 		ctx.stroke()
-		
+
 		nick = $("<div class='nick'>#{user.id}</div>").appendTo(avatar)
 		nick.css 'color', "rgba(#{user.color}, 1)"
-		
+
 		return $(avatar)
